1e8835d34d61e2e6f79047e601bccc77c42fe5eb
AdrianoDeshon <adriano.535230017@stu.untar.ac.id>
2024-09-19 13:23:46 +0700

    test commit

---
+++ shop/scriptshop.js
@@ -0,0 +1,7 @@
+// Add to Cart Button Functionality
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Item added to cart!');
+    });
+});
