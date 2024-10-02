06bb67f08aa54f81494b42beb1bfca0a5bfb67dd
joanneoliviaa <joanneolivia81@gmail.com>
2024-10-02 21:51:11 +0700

    a

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ document.getElementById("newsletterForm").addEventListener("submit", function(ev
         alert("Silakan masukkan email yang valid.");
     }
 });
+
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+
+function showSlide(index) {
+  if (index >= slides.length) slideIndex = 0;
+  if (index < 0) slideIndex = slides.length - 1;
+
+  slides.forEach(slide => slide.classList.remove('active'));
+  
+  dots.forEach(dot => dot.classList.remove('active-dot'));
+
+  slides[slideIndex].classList.add('active');
+  dots[slideIndex].classList.add('active-dot');
+}
+
+function moveSlide(step) {
+  slideIndex += step;
+  showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
+
+setInterval(() => {
+  moveSlide(1);
+}, 5000); 
